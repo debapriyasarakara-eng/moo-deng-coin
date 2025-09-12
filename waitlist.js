1903925391448b164e6bf5b8f0fa8af8957c2ffc
debapriyasarakara-eng <debapriyasarakara@gmail.com>
2025-09-12 23:29:33 +0530

    Create waitlist.js

---
+++ waitlist.js
@@ -0,0 +1,20 @@
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { app } from "./firebase-config.js";
+
+const db = getFirestore(app);
+
+const joinBtn = document.getElementById("join-btn");
+if (joinBtn) {
+    joinBtn.addEventListener("click", async () => {
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const message = document.getElementById("message");
+
+        try {
+            await addDoc(collection(db, "waitlist"), { name, email, timestamp: Date.now() });
+            message.innerText = "You are now on the waitlist!";
+        } catch (e) {
+            message.innerText = "Error joining waitlist.";
+        }
+    });
+}
