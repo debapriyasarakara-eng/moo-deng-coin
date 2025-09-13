d97c7b7e479c8b403827b8d17c6bcea91effdfc5
debapriyasarakara-eng <debapriyasarakara@gmail.com>
2025-09-13 08:34:37 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,15 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
+// This file should contain your actual Firebase project configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBRitTsyqXfk6XNh3t1j0boebQa1LNQkKs",
-  authDomain: "moo-deng-coin.firebaseapp.com",
-  projectId: "moo-deng-coin",
-  storageBucket: "moo-deng-coin.firebasestorage.app",
-  messagingSenderId: "629636349388",
-  appId: "1:629636349388:web:a9a7924aaea9354a09f169",
-  measurementId: "G-T7FVX8DW2Z"
+    apiKey: "AIzaSyBRitTsyqXfk6XNh3t1j0boebQa1LNQkKs",
+    authDomain: "moo-deng-coin.firebaseapp.com",
+    projectId: "moo-deng-coin",
+    storageBucket: "moo-deng-coin.firebasestorage.app",
+    messagingSenderId: "629636349388",
+    appId: "1:629636349388:web:a9a7924aaea9354a09f169",
+    measurementId: "G-T7FVX8DW2Z"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
+
