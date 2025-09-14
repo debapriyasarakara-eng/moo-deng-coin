5ba1af241ed04987ab446b278cae6828b462ba3b
debapriyasarakara-eng <debapriyasarakara@gmail.com>
2025-09-14 13:14:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,16 +4,20 @@ const urlsToCache = [
   "/index.html",
   "/manifest.json",
   "/coin.png",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
+  "/icon-192.png",
+  "/icon-512.png"
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+// Fetch from cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
+  );
 });
